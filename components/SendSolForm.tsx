1a9398707251c175f55f3ad50ee1ede6ae803600
maaax.eth <maxyay5566@gmail.com>
2022-11-20 22:03:19 +0800

    add send sol token instruction

--- components/SendSolForm.tsx
+++ components/SendSolForm.tsx
@@ -1,11 +1,35 @@
-import { FC } from 'react'
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import * as web3 from '@solana/web3.js'
+import { LAMPORTS_PER_SOL } from '@solana/web3.js';
+import { FC, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 
 export const SendSolForm: FC = () => {
 
+    const [ txSig, setTxSig ] = useState('');
+    const { connection } = useConnection();
+    const { publicKey, sendTransaction } = useWallet();
+    const link = () => {
+        return txSig ? `https://explorer.solana.com/tx/${txSig}?cluster=devnet` : ''
+    }
+
     const sendSol = event => {
         event.preventDefault()
+        if (!connection || !publicKey) { return }
+        const transaction = new web3.Transaction()
+        const recipientPubKey = new web3.PublicKey(event.target.recipient.value)
+
+        const sendSolInstruction = web3.SystemProgram.transfer({
+            fromPubkey: publicKey,
+            toPubkey: publicKey,
+            lamports: LAMPORTS_PER_SOL * event.target.amount.value
+        })
+
+        transaction.add(sendSolInstruction)
+        sendTransaction(transaction, connection).then(sig => {
+            setTxSig(sig)
+        })
         console.log(`Send ${event.target.amount.value} SOL to ${event.target.recipient.value}`)
     }
 
